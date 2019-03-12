57a21661b4e1b736bb4498ce259b882c534461f1
unknown <nrui@berkeley.edu>
2019-03-11 23:18:31 -0700

    Added mt pt 2

---
+++ homework/mt0b/mt0b.py
@@ -0,0 +1,72 @@
+"""ASTRONOMY 98/198 SPRING 2019"""
+"""MIDTERM PART 2: PLANETS"""
+"""NAME: """
+#YOUR SUBMISSION WILL BE A ZIP FILE WITH YOUR CODE (THIS FILE) AND YOUR PLOTS
+
+#STEP 0: Import the Modules You Will Need
+#(numpy, Table from astropy.table, and matplotlib.pyplot)
+
+#STEP 1: Read the planetdiscoveries.csv file
+#(Use the Table.read function from astropy.)
+
+#STEP 2: Find the average orbital period of the listed planets.
+#(This number should just be the arithmetic mean)
+#(You can preview the csv file using Excel to learn what the column names mean!)
+#Your UNITS should be in DAYS.
+#YOUR CODE HERE
+
+"""
+The average orbital period of the listed planets is <your number here>.
+"""
+
+#STEP 3: Find the average ratio of planet radius to stellar radius; e.g. what is the average value of
+#(planet radius / stellar radius)?
+#(This number should just be the arithmetic mean)
+#(Make sure your units are consistent! You can preview the csv file using Excel to learn what the units are!)
+#YOUR CODE HERE
+
+"""
+The average ratio of planet radius to stellar radius of the listed planets is <your number here>.
+"""
+
+#STEP 4: Plot Types of Detection vs Year of Discovery Bar Graph; Include Error Bars
+#YOUR CODE HERE
+#Remember to submit your plot
+
+#STEP 5: Radius vs Period Plot Differentiated by Planet Detection Method; Include Error Bars
+#YOUR CODE HERE
+#Remember to submit your plot
+
+
+#STEP 6: Answer Questions
+#What detection method has been the most prolific?
+#YOUR CODE HERE: Use the length function and give us a number.
+"""
+YOUR ANSWER HERE
+"""
+
+#What detection method has been the second most prolific?
+#YOUR CODE HERE: Use the length function and give us a number.
+"""
+YOUR ANSWER HERE
+"""
+
+#What portions of parameter space are covered by the detection methods?
+"""
+YOUR ANSWER HERE
+(hint: use your plots)
+"""
+
+#What are some shortcomings of the radial velocity and transit methods?
+"""
+YOUR ANSWER HERE
+"""
+#Hint: Background Reading:
+#https://www.cfa.harvard.edu/~avanderb/tutorial/tutorial.html
+#http://www.planetary.org/explore/space-topics/exoplanets/transit-photometry.html
+#http://www.planetary.org/explore/space-topics/exoplanets/radial-velocity.html
+
+#STEP 7: Reflections
+"""
+SOME REFLECTIONS FROM THIS PROJECT?
+"""
